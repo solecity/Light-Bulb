0c4e1696a9563aefe668329535da0902af87f733
axemm99 <inesmm1999@gmail.com>
2019-04-23 14:23:02 +0100

    edits

--- src/components/GestorCourses.vue
+++ src/components/GestorCourses.vue
@@ -2,54 +2,40 @@
   <div>
     <b-form @submit="onSubmit" @reset="onReset" v-if="show">
       <b-form-group
-        id="input-group-1"
-        label="Email address:"
-        label-for="input-1"
-        description="We'll never share your email with anyone else."
+        id="nameLabel"
+        label="Nome do Curso:"
+        label-for="inputName"
       >
         <b-form-input
-          id="input-1"
+          id="inputName"
           v-model="form.email"
-          type="email"
+          type="text"
           required
-          placeholder="Enter email"
+          placeholder="Nome do Curso"
         ></b-form-input>
       </b-form-group>
 
       <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
-        <b-form-input
-          id="input-2"
-          v-model="form.name"
-          required
-          placeholder="Enter name"
-        ></b-form-input>
+        <b-form-input id="input-2" v-model="form.name" required placeholder="Enter name"></b-form-input>
       </b-form-group>
 
       <b-form-group id="input-group-3" label="Food:" label-for="input-3">
-        <b-form-select
-          id="input-3"
-          v-model="form.food"
-          :options="foods"
-          required
-        ></b-form-select>
-      </b-form-group>
-
-      <b-form-group id="input-group-4">
-        <b-form-checkbox-group v-model="form.checked" id="checkboxes-4">
-          <b-form-checkbox value="me">Check me out</b-form-checkbox>
-          <b-form-checkbox value="that">Check that out</b-form-checkbox>
-        </b-form-checkbox-group>
+        <b-form-select id="input-3" v-model="form.food" :options="foods" required></b-form-select>
       </b-form-group>
 
       <b-button type="submit" variant="primary">Submit</b-button>
       <b-button type="reset" variant="danger">Reset</b-button>
     </b-form>
-    <b-card class="mt-3" header="Form Data Result">
-      <pre class="m-0">{{ form }}</pre>
-    </b-card>
-
+    <br>
     <div>
-        <b-table striped hover :items="items" :fields="fields" :bordered="bordered" :outlined="outlined"></b-table>
+      <b-table
+        striped
+        hover
+        :items="items"
+        :fields="fields"
+        :bordered="bordered"
+        :outlined="outlined"
+      ></b-table>
     </div>
   </div>
 </template>
@@ -63,19 +49,25 @@ export default {
   data: function() {
     return {
       form: {
-          "email": "",
-          "name": "",
-          "food": null,
-          "checked": []
+        email: "",
+        name: "",
+        food: null,
+        checked: []
       },
-        items: [
-          { id: 40, username: 'Dickerson', block: `<i class="fas fa-lock"></i>` },
-          { id: 21, username: 'Larsen', block: '<i class="fas fa-lock"></i>' },
-          { id: 89, username: 'Geneva', block: '<i class="fas fa-lock"></i>' },
-          { id: 38, username: 'Jami', block: '<i class="fas fa-lock"></i>' }
-        ],
-        foods: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
-        show: true
+      items: [
+        { id: 40, username: "Dickerson", block: `<i class="fas fa-lock"></i>` },
+        { id: 21, username: "Larsen", block: '<i class="fas fa-lock"></i>' },
+        { id: 89, username: "Geneva", block: '<i class="fas fa-lock"></i>' },
+        { id: 38, username: "Jami", block: '<i class="fas fa-lock"></i>' }
+      ],
+      foods: [
+        { text: "Select One", value: null },
+        "Carrots",
+        "Beans",
+        "Tomatoes",
+        "Corn"
+      ],
+      show: true
     };
   },
   created() {
@@ -88,26 +80,24 @@ export default {
   },
   methods: {
     onSubmit(evt) {
-      evt.preventDefault()
-      alert(JSON.stringify(this.form))
+      evt.preventDefault();
+      alert(JSON.stringify(this.form));
     },
-      onReset(evt) {
-        evt.preventDefault()
-        // Reset our form values
-        this.form.email = ''
-        this.form.name = ''
-        this.form.food = null
-        this.form.checked = []
-        // Trick to reset/clear native browser form validation state
-        this.show = false
-        this.$nextTick(() => {
-        this.show = true
-      })
+    onReset(evt) {
+      evt.preventDefault();
+      // Reset our form values
+      this.form.email = "";
+      this.form.name = "";
+      this.form.food = null;
+      this.form.checked = [];
+      // Trick to reset/clear native browser form validation state
+      this.show = false;
+      this.$nextTick(() => {
+        this.show = true;
+      });
     }
   },
-  computed: {
-    
-  }
+  computed: {}
 };
 </script>
 

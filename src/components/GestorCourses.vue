2554a3b8cbe200ef4188a0ea9f0d4372850daa84
axemm99 <inesmm1999@gmail.com>
2019-04-23 15:04:36 +0100

    edit

--- src/components/GestorCourses.vue
+++ src/components/GestorCourses.vue
@@ -19,7 +19,7 @@
         <b-form-select id="input-3" v-model="form.unit" :options="setUnits" required multiple="true"></b-form-select>
       </b-form-group>
 
-      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="submit" variant="primary">Adicionar</b-button>
       <b-button type="reset" variant="danger">Reset</b-button>
     </b-form>
     <br>

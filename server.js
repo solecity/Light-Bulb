6062b81445f926f16a626a2b14dd287bf0b1c344
solecity <solecity@hotmail.com>
2019-06-15 14:13:28 +0100

    heroku

---
+++ server.js
@@ -0,0 +1,20 @@
+
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 8080;
+
+const mainRoutes = require("./routes/main.routes.js");
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use(mainRoutes);
+
+app.listen(port, function(port) {
+    console.log("Server is running. Port: ", port);
+});
